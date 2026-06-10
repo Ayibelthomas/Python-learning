464c59f0087287224a86153d46ea7af59af55d0a
Ayibel Thomas <ayibelthomas@gmail.com>
2026-06-10 21:16:36 +0530

    Added new python file

---
+++ first_pythoncode.py
@@ -0,0 +1,17 @@
+name = 'Ayibel Thomas' # This is a string 
+age = 23 # This is an integer
+salary = 16445.45 # This is a float
+is_working = True # this is a boolen
+experience = ['Python','java','c++'] # this is a list
+companys = ('Google','Meta','Amazon') # this is a tuple
+skills = {'Python','java','c++'} # this is a set
+domains = {"Database" : "SQL","Web Development":"Django","Data Science":"Pandas"} # this is a dictionary
+if (is_working ==True ):
+    print(f"The name of the employee is {name} \n Age is {age}\n and the salary is {salary}")
+else:
+    print(f"{name} is not working")
+print(experience)
+print(companys)
+print(skills)
+print(domains)
+print(type(name),type(age),type(salary),type(is_working),type(experience),type(companys),type(skills),type(domains))
