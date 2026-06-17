99179d54ff7f62d98f5985eb3d8113d07e64bf9a
Ayibelthomas <96002523+Ayibelthomas@users.noreply.github.com>
2026-06-17 12:27:21 +0530

    Create Exception.py
    This is a program that shows the demonstration of using exception handling with else and finally .

---
+++ Exception.py
@@ -0,0 +1,16 @@
+def division(a,b):
+  try:
+    c = a/b
+  except Exception as e :
+    return f"{e} error !!"
+  else :
+    print("There was no error occured!!")
+  finally:
+    print(f"There we divied {a} with {b}")
+
+try :
+  a = int(input('Enter a number :'))
+  b = int(input('Enter a second number :'))
+  print(f'We get {division(a,b)}')
+except Exception as e :
+  print(e)
