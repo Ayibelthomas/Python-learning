3314fad233d9fa3b87882222f92e544dd70c7c32
Ayibelthomas <96002523+Ayibelthomas@users.noreply.github.com>
2026-07-01 13:06:28 +0530

    Create writing_file.py
    writing the file in python.

---
+++ writing_file.py
@@ -0,0 +1,5 @@
+file = open("John Snow" "w")
+string = '''John Snow is a character from a fictional story named game of thornes.
+He is the lead character in this show.'''
+file.write(string)
+file.close()
