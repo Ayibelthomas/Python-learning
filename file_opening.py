737a21bdc7af97ea2efd24037586e7dbb740cb9f
Ayibelthomas <96002523+Ayibelthomas@users.noreply.github.com>
2026-07-01 13:25:16 +0530

    Update file_opening.py

--- file_opening.py
+++ file_opening.py
@@ -8,3 +8,7 @@ file2 = open("READ.md","r")
 for line in file2 :
   print(line.strip())
 file2.close()
+print('.........................................')
+with open('README.md','r') as file :  #Here we are using "with" for reading a file and it will close file automatically
+  content = file.read()
+  print(content)
